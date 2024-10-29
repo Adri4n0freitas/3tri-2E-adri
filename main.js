140b9e576e865244e894e4edec18020fd906ddd1
Adri4n0freitas <160138847+Adri4n0freitas@users.noreply.github.com>
2024-10-29 08:51:34 -0300

    29/10

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ function criaCartao(categoria,pergunta,resposta){
     let container = document.getElementById("container")
     let article =document.createElement("article")
     article.className = "cartao"
-    article.innerHTML = "Isso é um teste"
+    article.innerHTML = `
+     <div class="cartao-conteudo">
+    <h3> ${categoria}</h3>
+    <div class="cartao-conteudo-pergunta">
+    <p> ${pergunta}</p>
+    </div>
+    <div class="cartao-conteudo-resposta">
+    <p> ${resposta}</p>
+    </div>
+     </div>
+    `
     container.appendChild(article)
 }
-criaCartao("Biologia","Qual a função da Mitocondria,R:Produzir ATP(Energia)")
+criaCartao("Biologia","Qual a função da Mitocondria?", "R:Produzir ATP (Energia)")
+criaCartao("Geral","Porque o Messi é melhor do que o CR7?","R:Porque ele tem 8 bolas de ouro.")
